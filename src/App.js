e3388e2e4a2a0d9b8e181616212c1b68eaff928e
이시형 <tlgud585@gmail.com>
2022-08-21 19:10:39 +0900

    UPDATE : isLoggedIn 경로 설정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import {Routes, Route, useNavigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Join from "./pages/join/Join";
 import Login from "./pages/login/Login";
+import Todo from "./pages/todo/Todo";
+import AuthContext from "./store/auth_context";
 import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
+	const navigate = useNavigate()
+	const {isLoggedIn} = useContext(AuthContext)
+	useEffect(()=>{
+		if (isLoggedIn) {
+			navigate("/todo")
+		}
+	},[navigate,isLoggedIn])
 	return (
-		<BrowserRouter>
+		<>
 			<GlobalStyles />
 			<Routes>
 				<Route path="/" element={<Layout />}>
 					<Route index element={<Login />} />
 					<Route path="join" element={<Join />} />
+					<Route path="todo" element={<Todo />} />
 				</Route>
 			</Routes>
-		</BrowserRouter>
+		</>
 	);
 }
 
